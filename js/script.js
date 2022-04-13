e2cd59c19426d8b2ba61e25680b58f5a2b7c5727
Edgar Gomes <egomes2013@outlook.com>
2022-04-13 18:01:14 -0300

    :up:

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
     $('.scrollspy').scrollSpy();
+
+
+
+
+
+    /* SCROLLFIRE */
+    var options = [
+        {selector: '.cards', offset: 200, callback: customCallbackFunc },
+        {selector: '.other-class', offset: 200, callback: function() {
+          customCallbackFunc();
+        } },
+      ];
+      Materialize.scrollFire(options);
 })
+
+
